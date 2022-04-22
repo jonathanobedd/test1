04a7397f7922fb73b88d307e75ddad3612f19506
obed <obed@MSI>
2022-04-22 16:07:03 +0700

    Nomor 1

---
+++ Test Suites/Test_Suite.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ff1d94ad-0bec-47cf-98e3-5f54a375c9dd</testSuiteGuid>
+   <testCaseLink>
+      <guid>9ae0ce95-f02f-436b-934c-6f471a0db8b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get List Users</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>caf5cd4e-b7f3-4056-a241-2f843de39bab</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>78b6301a-6605-472b-b8fd-93b3a6b18744</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Post Register Successful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>21a1c7d8-4cd6-43ce-bfb9-3932356957df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Single Users</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f809d874-2e16-435b-b09b-da6912f648d0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PUT update</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
